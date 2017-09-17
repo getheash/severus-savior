37d3ee451ac2eb4879ccd70630f1500229fe3c55
Ashwin <mashven@gmail.com>
2017-09-17 12:23:30 +0530

    #trying out git plus

--- C/hello.c
+++ C/hello.c
@@ -6,5 +6,6 @@ int main()
     printf("does it?\n");
     printf("adding new branch\n");
     printf("lets see if i can add one more commit to merge with main branch\n");
+    printf("I have successfully created a new branch, made a commit, merged to main branch and made a push\n");
     return 0;
 }
