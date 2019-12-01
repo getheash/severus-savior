709be34439ea0cb0cde724a9f0cd9791d4def06b
gethash <mashven@gmail.com>
2019-12-01 13:33:23 +0530

    making second commit on a new branch

--- CPP/test.cpp
+++ CPP/test.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main()
 {
   cout<<"Hello World\n";
-  cout<<"making my first commit";
+  cout<<"making my first commit\n";
+  cout<<"adding code to make second commit";
   return 0;
 }
